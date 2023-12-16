6c2ed097e56d6d2bbed59915f91263510d24b2ba
Chethan K C <135944074+ChethanKC02@users.noreply.github.com>
2023-12-16 07:56:18 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let displayValue = '';
-
+let smallDisplayValue = '';
 function appendToDisplay(value) {
   displayValue += value;
   document.getElementById('display').value = displayValue;
@@ -10,22 +10,24 @@ function clearDisplay() {
   document.getElementById('display').value = displayValue;
 }
 
-function removeLast(){  
+function removeLast() {
   let n = displayValue.length;
-  if(n != 0){
-    displayValue = displayValue.slice(0,-1);
-    document.getElementById('display').value= displayValue;
+  if (n != 0) {
+    displayValue = displayValue.slice(0, -1);
+    document.getElementById('display').value = displayValue;
   }
 }
 
 function calculate() {
   try {
     if (displayValue != '') {
+      document.getElementById('small-display').value = displayValue + " =";
       displayValue = eval(displayValue);
       document.getElementById('display').value = displayValue;
     }
     else {
-      document.getElementById('display').value = "No Input Value  !";
+      document.getElementById('small-display').value = "";
+      document.getElementById('display').value = "";
     }
   } catch (error) {
     document.getElementById('display').value = 'Error';
